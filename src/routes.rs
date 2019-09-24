2fe5fdb8a0c78fb082de6d3bcd52a4afd46ccf1b
Justin Nguyen <justin.ntt@gmail.com>
2019-09-24 04:58:49 +0100

    refactor

--- src/routes.rs
+++ src/routes.rs
@@ -1,20 +1,22 @@
 use actix_web::{web, App, HttpServer};
 
+use crate::handlers as handlers;
+
 pub fn config() {
   HttpServer::new(|| { App::new()
     .service(
-      web::resource("/").route(web::get().to_async(crate::handlers::home::index))
+      web::resource("/").route(web::get().to_async(handlers::home::index))
     )
     .service(
       web::resource("/products")
-        .route(web::get().to_async(crate::handlers::products::index))
-        .route(web::post().to_async(crate::handlers::products::create))
+        .route(web::get().to_async(handlers::products::index))
+        .route(web::post().to_async(handlers::products::create))
     )
     .service(
       web::resource("/products/{id}")
-        .route(web::get().to_async(crate::handlers::products::show))
-        .route(web::delete().to_async(crate::handlers::products::destroy))
-        .route(web::patch().to_async(crate::handlers::products::update))
+        .route(web::get().to_async(handlers::products::show))
+        .route(web::delete().to_async(handlers::products::destroy))
+        .route(web::patch().to_async(handlers::products::update))
     )
   })
   .bind("127.0.0.1:8000")
